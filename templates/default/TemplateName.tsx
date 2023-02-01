1b59a91c1d7c98a0c6bea14e66cc34b3f0eade9a
Aleksandr Zautsev <alexandr.falkone@yandex.ru>
2023-02-01 14:31:30 +0300

    first commit

---
+++ templates/default/TemplateName.tsx
@@ -0,0 +1,7 @@
+import styles from './TemplateName.module.scss';
+
+interface TemplateNameProps {}
+
+export const TemplateName = ({}: TemplateNameProps) => (
+  <div className={styles.templateName}>TemplateName Component</div>
+);
